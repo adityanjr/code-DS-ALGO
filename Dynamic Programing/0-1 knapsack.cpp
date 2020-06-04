2cc389d77653017d0324211030d4ad0fd01832e4
Aditya Singh <samant04aditya@gmail.com>
2020-06-04 08:35:00 +0530

    ...

--- Dynamic Programing/0-1 knapsack.cpp
+++ Dynamic Programing/0-1 knapsack.cpp
@@ -1,45 +1,71 @@
-#include<bits/stdc++.h>
-
-#define int long long int
-#define F first
-#define S second
-#define pb push_back
-
+//0-1 Knapsack problem - Dynamic programming
+//#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
-const int N = 100005;
+//void Print(int res[20][20], int i, int j, int capacity)
+//{
+//	if(i==0 || j==0)
+//	{
+//		return;
+//	}
+//	if(res[i-1][j]==res[i][j-1])
+//	{
+//		if(i<=capacity)
+//		{
+//			cout<<i<<" ";
+//		}
+//
+//		Print(res, i-1, j-1, capacity-i);
+//	}
+//	else if(res[i-1][j]>res[i][j-1])
+//	{
+//		Print(res, i-1,j, capacity);
+//	}
+//	else if(res[i][j-1]>res[i-1][j])
+//	{
+//		Print(res, i,j-1, capacity);
+//	}
+//}
 
-int32_t main()
+int Knapsack(int capacity, int n, int weight[], int value[])
 {
-    {
-        int i, j, k, n, m, ans = 0, cnt = 0, sum = 0;
-        cin >> n >> m;
-        int size[n + 1], value[n + 1];
-        for (i = 1; i <= n; i++) {
-            cin >> size[i];
-        }
-        for (i = 1; i <= n; i++) {
-            cin >> value[i];
-        }
-        int dp[n + 1][m + 1] = {};
-        for (i = 1; i <= n; i++) {
-            for (j = 0; j <= m; j++) {
-                if (j - size[i] >= 0) {
-                    dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - size[i]] + value[i]);
-                }
-                else {
-                    dp[i][j] = dp[i - 1][j];
-                }
-                if (j > 0) {
-                    dp[i][j] = max(dp[i][j], dp[i][j - 1]);
-                }
-            }
-        }
-        // for(i=0;i<=n;i++){
-        //     for(j=0;j<=m;j++){
-        //         cout<<dp[i][j]<<" ";
-        //     }cout<<'\n';
-        // }
-        cout << dp[n][m];
-    }
+	int res[20][20];
+	for (int i = 0; i < n + 1; ++i)
+	{
+		for (int j = 0; j < capacity + 1; ++j)
+		{
+			if (i == 0 || j == 0)
+				res[i][j] = 0;
+			else if (weight[i - 1] <= j)
+				res[i][j] = max(value[i - 1] + res[i - 1][j - weight[i - 1]], res[i - 1][j]);
+			else
+				res[i][j] = res[i - 1][j];
+		}
+	}
+	//	Print(res, n, capacity, capacity);
+	//	cout<<"\n";
+	return res[n][capacity];
+}
+int main()
+{
+	int n;
+	cout << "Enter number of items: ";
+	cin >> n;
+	int weight[n], value[n];
+	cout << "Enter weights: ";
+	for (int i = 0; i < n; ++i)
+	{
+		cin >> weight[i];
+	}
+	cout << "Enter values: ";
+	for (int i = 0; i < n; ++i)
+	{
+		cin >> value[i];
+	}
+	int capacity;
+	cout << "Enter capacity: ";
+	cin >> capacity;
+	cout << Knapsack(capacity, n, weight, value);
+	return 0;
 }
