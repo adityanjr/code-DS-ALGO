315ec9764b1527a143eb88997dd5098feb7d6649
swoyamsss <64949434+swoyamsss@users.noreply.github.com>
2020-10-04 15:44:00 +0530

    Update 14.isomerphism of 2 binary trees.cpp

--- Binary Tree/14.isomerphism of 2 binary trees.cpp
+++ Binary Tree/14.isomerphism of 2 binary trees.cpp
@@ -17,23 +17,23 @@ struct Node* create(int n)
 
  return temp;
 }
-
+//isomerphism means either 2 trees are the same or mirror image of each other
 bool check(struct Node*root1,struct Node* root2)
 {
-    if(root1==NULL&&root2==NULL)
+    if(root1==NULL&&root2==NULL)//checks for both are null
     {
         return true;
     }
-    else if(root1==NULL||root2==NULL)
+    else if(root1==NULL||root2==NULL)//checks for if one of the roots is null
     {
         return false;
     }
-    else if(root1->data!=root2->data)
+    else if(root1->data!=root2->data)//if at the roor node the data is different then they cannot be isomers
     {
         return false;
     }
     else if((check(root1->left,root2->left)&&(check(root1->right,root2->right)))||(check(root1->left,root2->right)&&(check(root1->right,root2->left))))
-    {
+    {//the 1st part before or checks for the if the trees are same and the condition after or checks if the trees are mirror images of each other
         return true;
     }
 }
@@ -48,7 +48,7 @@ int main()
     root2 = create(1);
     root2->left = create(3);
     root2->right = create(2);
-    if(check(root1,root2)==true)
+    if(check(root1,root2)==true)//function to check the trees
     {
         cout<<"they are isomers\n";
     }
