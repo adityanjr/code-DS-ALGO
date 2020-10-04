fae1fc4fd9ce1f5ea61d2da77f3652c1a8175e39
swoyamsss <64949434+swoyamsss@users.noreply.github.com>
2020-10-04 15:46:33 +0530

    Update 15. print the path with given sum.cpp

--- Binary Tree/15. print the path with given sum.cpp
+++ Binary Tree/15. print the path with given sum.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 int s = 0;
-stack<struct Node*> st;
+stack<struct Node*> st;//for storing the nodes
 struct Node
 {
     int data;
@@ -18,7 +18,7 @@ struct Node* create(int n)
 
  return temp;
 }
-int sum(struct Node* root,int k)
+int sum(struct Node* root,int k)//inorder traversal is used for this algorithm
 {
     if(root==NULL)
     {
@@ -26,7 +26,7 @@ int sum(struct Node* root,int k)
     }
     s+=root->data;
     st.push(root);
-    if(s==k)
+    if(s==k)//k is the required sum provided by the user
     {
         while(!st.empty())
         {
@@ -39,7 +39,7 @@ int sum(struct Node* root,int k)
     sum(root->left,k);
     sum(root->right,k);
     s-=root->data;
-    st.pop();
+    st.pop();//the top element is poped out as we are moving backwards
     return 0;
 }
 int main()
