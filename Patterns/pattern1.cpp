0d2c44ea105bc2807f2547e2e22416924ae9001a
adityaDTU <69716979+adityaDTU@users.noreply.github.com>
2022-10-11 11:06:02 +0530

    Update pattern1.cpp

--- Patterns/pattern1.cpp
+++ Patterns/pattern1.cpp
@@ -5,10 +5,9 @@ int main(){
 
 	int n;
 	cin >> n;
-
-	for( int i = 0; i < n; i++ ){
-		for( int j = 0; j < n; j++ ){
-			cout << "*";
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < n; j++){
+			cout << "* ";
 		}
 		cout << endl;
 	}
